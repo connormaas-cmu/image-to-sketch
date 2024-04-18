d388e12557b3c0f56cc8c796273ad0b148c47e2c
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-04-18 18:17:20 -0400

    testing edge function

--- netlify/edge-functions/caption/caption.js
+++ netlify/edge-functions/caption/caption.js
@@ -1,45 +1,53 @@
-async function setupFetch() {
-  const module = await import('node-fetch');
-  return module.default;
-}
+export default async (request) =>
+  new Response(JSON.stringify({test: 'Hello, World!'}), {
+    headers: { 'content-type': 'text/json' },
+  })
 
-export default async (request) => {
-  const fetch = await setupFetch();
-
-  try {
-
-    return Response.json({test: "Hello!!"})
+  export const config = { path: "/test" };
 
 
-    const API_KEY = process.env.API_KEY;
-    const API_HOST = 'open-ai21.p.rapidapi.com';
+// async function setupFetch() {
+//   const module = await import('node-fetch');
+//   return module.default;
+// }
 
-    const body = JSON.parse(request.body);
-    const imageBase64 = body.image;
+// export default async (request) => {
+//   const fetch = await setupFetch();
 
-    const buff = Buffer.from(imageBase64, 'base64');
-    const image = new Blob([buff.buffer], { type: 'image/png' });
+//   try {
 
-    const data = new FormData();
-    data.append('file', image);
+//     return Response.json({test: "Hello!!"})
 
-    const response = await fetch(`https://open-ai21.p.rapidapi.com/imagecaptioning`, {
-      method: 'POST',
-      headers: {
-        'X-RapidAPI-Key': API_KEY,
-        'X-RapidAPI-Host': API_HOST
-      },
-      body: data
-    });
 
-    return new Response(response.text(), { status: 200 })
+//     const API_KEY = process.env.API_KEY;
+//     const API_HOST = 'open-ai21.p.rapidapi.com';
 
-    const result = await response.json();
+//     const body = JSON.parse(request.body);
+//     const imageBase64 = body.image;
 
-  } catch (error) {
-    // Return an error response
-    return new Response(error.toString(), { status: 500 });
-  }
-};
+//     const buff = Buffer.from(imageBase64, 'base64');
+//     const image = new Blob([buff.buffer], { type: 'image/png' });
 
-export const config = { path: "/test" };
+//     const data = new FormData();
+//     data.append('file', image);
+
+//     const response = await fetch(`https://open-ai21.p.rapidapi.com/imagecaptioning`, {
+//       method: 'POST',
+//       headers: {
+//         'X-RapidAPI-Key': API_KEY,
+//         'X-RapidAPI-Host': API_HOST
+//       },
+//       body: data
+//     });
+
+//     return new Response(response.text(), { status: 200 })
+
+//     const result = await response.json();
+
+//   } catch (error) {
+//     // Return an error response
+//     return new Response(error.toString(), { status: 500 });
+//   }
+// };
+
+// export const config = { path: "/test" };
