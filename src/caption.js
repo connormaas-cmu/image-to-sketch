61ac6d37921715e93d5b6a36493406c7b12595b1
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-04-18 18:45:47 -0400

    testing edge function

--- src/caption.js
+++ src/caption.js
@@ -2,32 +2,27 @@ function captionImage(image) {
 
     return new Promise((resolve, reject) => { 
 
-        fetch('https://sketch2image.netlify.app/test')
-        .then(response => alert(response))
-        .catch(error => alert(error))
-  
-
-        // fetch('https://sketch2image.netlify.app/test', {
-        //     method: 'GET',
-        //   //  headers: { 'Content-Type': 'application/json' },
-        //   //  body: JSON.stringify({ image: image }),
-        // })
-        // .then(response => response.text())
-        // .then(textResponse => {
-        //     alert(textResponse);
-        //     if (textResponse.includes("Error")) {
-        //         alert(textResponse)
-        //         return;
-        //     }
-        //     // const data = JSON.parse(textResponse);
+        fetch('https://sketch2image.netlify.app/test', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+            body: JSON.stringify({ image: image }),
+        })
+        .then(response => response.text())
+        .then(textResponse => {
+            alert(textResponse);
+            if (textResponse.includes("Error")) {
+                alert(textResponse)
+                return;
+            }
+            // const data = JSON.parse(textResponse);
             
-        //     resolve(textResponse);       
+            resolve(textResponse);       
 
-        // })
-        // .catch(error => {
-        //     alert(error);
-        //     reject(error);
-        // });
+        })
+        .catch(error => {
+            alert(error);
+            reject(error);
+        });
     });
 }
 
