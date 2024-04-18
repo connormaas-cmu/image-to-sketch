4de7604650df5e1ea57f2252c75d839b4a4889ee
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-04-18 09:41:15 -0400

    added sort of styled drawing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import React, { useState, useRef } from 'react';
 
 
 function App() {
+  const [oldColor, setOldColor] = useState('#444'); 
   const [color, setColor] = useState('#444'); 
   const [size, setSize] = useState(10); 
   const [imageURL, setImageURL] = useState('');
   const canvasRef = useRef(null);
+  const [eraserEnabled, setEraserEnabled] = useState(false);
 
   const clearCanvas = () => {
     canvasRef.current.clear();
@@ -18,16 +20,33 @@ function App() {
     setImageURL(dataUrl);
   };
 
+  const toggleEraser = () => {
+    setOldColor(color)
+    setEraserEnabled(!eraserEnabled);
+    setColor(eraserEnabled ? oldColor : '#FFFFFF');
+  };
+
+  const undoLastAction = () => {
+    canvasRef.current.undo();
+  };
+
+
   return (
     <div className="App">
-     <CanvasDraw ref={canvasRef} brushColor={color} brushRadius={size} />
-      <input type="color" value={color} onChange={e => setColor(e.target.value)} />
-      <input type="range" min="1" max="20" value={size} onChange={e => setSize(e.target.value)} />
-      <button onClick={clearCanvas}>Clear</button>
-      <button onClick={saveDrawing}>Save Drawing</button>
-      {imageURL && <div>
-        <a href={imageURL} target="_blank" rel="noopener noreferrer">Open Saved Drawing</a>
-      </div>}
+      <div className="canvasContainer">
+        <CanvasDraw ref={canvasRef} lazyRadius={0} hideGrid={true} brushColor={color} brushRadius={size} className="canvasStyle" />
+        {!eraserEnabled && (
+          <input type="color" value={color} onChange={e => setColor(e.target.value)} />
+        )}
+        <input type="range" min="1" max="20" value={size} onChange={e => setSize(e.target.value)} />
+        <button onClick={toggleEraser}>{eraserEnabled ? 'Back to Pen' : 'Eraser'}</button>
+        <button onClick={clearCanvas}>Clear</button>
+        <button onClick={undoLastAction}>Undo</button>
+        <button onClick={saveDrawing}>Save Drawing</button>
+        {imageURL && <div>
+          <a href={imageURL} target="_blank" rel="noopener noreferrer">Open Saved Drawing</a>
+        </div>}
+      </div>
     </div>
   );
 }
