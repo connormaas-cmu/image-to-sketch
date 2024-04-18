2e524d4b98795b74d111f4d9fa054a4466aeddd1
connormaas-cmu <cmaas@andrew.cmu.edu>
2024-04-18 15:14:17 -0400

    debug

--- netlify/functions/caption/caption.js
+++ netlify/functions/caption/caption.js
@@ -9,9 +9,10 @@ const handler = async (event) => {
     const API_KEY = process.env.API_KEY;
     const API_HOST = 'open-ai21.p.rapidapi.com';
 
-    throw new Error(JSON.stringify(event))
+    const body = JSON.parse(event.body);
+    const imageBase64 = body.image;
 
-    const imageBase64 = event.queryStringParameters.image;
+    throw new Error(imageBase64)
 
     const buff = Buffer.from(imageBase64, 'base64');
     const image = new Blob([buff.buffer], { type: 'image/png' });
